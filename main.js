7c754d21a8019a71102342603d5b84e1b2680e3e
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2025-07-15 08:08:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var num1=prompt("ENTER NUM 1");
+var num2=prompt("ENTER NUM 2");
+var num3=prompt("ENTER NUM 3");
+
+
+function arr(num1){
+    var store=[num1,num2,num3];
+    console.log(store);
+}
+
+arr(num1,num2,num3);
